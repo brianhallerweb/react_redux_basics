8bf50401b8d6308733137fa32b8c2bbe9338e196
Brian Haller <brianhaller@Brians-MacBook-Pro.local>
2018-05-29 17:48:29 -0700

    commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/styles.scss";
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+ReactDOM.render(<p>Hello World</p>, document.getElementById("app"));
